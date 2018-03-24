7c17e37a2d0dfece6f7c211dfc61201e5246d0ab
danielcampos <ddregalo@gmail.com>
2018-03-24 09:57:53 +0000

    Add search city weather feature

--- src/interface.js
+++ src/interface.js
@@ -1,6 +1,5 @@
 $(document).ready( function() {
-
-  var city = 'London'
+  var findCity = "London";
   var thermostat = new Thermostat();
   showTemp();
 
@@ -29,12 +28,36 @@ $(document).ready( function() {
     showTemp();
   });
 
+  $('#findcity').submit(function(event) {
+    event.preventDefault();
+    var findCity = $('#city').val();
+    loadCityTemp(findCity);
+    $('#displaycity').text(`The Weather in ${findCity} is  `);
+  });
+
   function showTemp() {
     $('#tempdisplay').text(`${thermostat.temperature}` + "℃");
     $('#tempdisplay').attr("class", thermostat.energyUsage());
   };
 
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city +'&units=metric&APPID=c34768cec566dee714d9eb7c3c2bed97', function(city) {
-    $('#weather').html(city.main['temp'])
-  });
+  function loadCityTemp(city) {
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city +'&units=metric&APPID=c34768cec566dee714d9eb7c3c2bed97', function(city) {
+      $('#weather').html(city.main['temp'] + "℃");
+    });
+  };
 });
+
+//
+//
+// $('#myForm').submit(function() {
+//     // get all the inputs into an array.
+//     var $inputs = $('#myForm :input');
+//
+//     // not sure if you wanted this, but I thought I'd add it.
+//     // get an associative array of just the values.
+//     var values = {};
+//     $inputs.each(function() {
+//         values[this.name] = $(this).val();
+//     });
+//
+// });
